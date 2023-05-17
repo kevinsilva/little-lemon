2870030b7643e3c09fa76e9f18717d24525fddba
kevinsilva <kevinsilva.j@gmail.com>
2023-05-17 19:42:36 +0100

    adds styles to confimation page

--- src/components/ConfirmationPage.js
+++ src/components/ConfirmationPage.js
@@ -1,31 +1,48 @@
+import { Link } from 'react-router-dom';
 import restaurant from '../assets/restaurant.jpg';
+import '../styles/BookingPage.css';
 
 function ConfirmationPage() {
   const formData = JSON.parse(localStorage.getItem('formData'));
   return (
-    <div>
-      <div className="confirmation-header">
-        <h2>We have received your confirmation</h2>
-        <p>
-          Please wait for the confirmation email you'll receive at
-          <span>&nbsp;{formData.email}</span>.
-        </p>
-        <img src={restaurant} alt="restaurant" />
-      </div>
-      <div className="confirmation-details">
-        <h3>Your reservation</h3>
-        <p>
-          <span>{formData.name}</span>
-        </p>
-        <p>
-          <span>{formData.date}&nbsp;</span>
-          <span>{formData.time}</span>
-        </p>
-        <p>
-          <span>{formData.numberOfGuests}&nbsp;</span>
-          <span>people</span>
-        </p>
-        <p>{formData.bookingOccasion}</p>
+    <div className="confirmation">
+      <div className="confirmation-container">
+        <div className="confirmation-header">
+          <h2 className="confirmation-title">
+            We have received your confirmation
+          </h2>
+          <p className="confirmation-message">
+            Please wait for the confirmation email you'll receive at
+            <span>&nbsp;{formData.email}</span>.
+          </p>
+          <img
+            className="confirmation-photo"
+            src={restaurant}
+            alt="restaurant"
+          />
+        </div>
+        <div className="confirmation-details">
+          <h3 className="details-title">Your reservation</h3>
+          <p>
+            <span>{formData.name}</span>
+          </p>
+          <p>
+            <span>{formData.date} at&nbsp;</span>
+            <span>{formData.time}</span>
+          </p>
+          <p>
+            <span>{formData.numberOfGuests}&nbsp;</span>
+            <span>people</span>
+          </p>
+          <p>{formData.bookingOccasion}</p>
+        </div>
+        <Link
+          to="/"
+          className="primary-button primary-button--yellow"
+          aria-label="homepage button"
+        >
+          Go To Homepage
+        </Link>
       </div>
     </div>
   );
--- src/components/ConfirmedBooking.js
+++ src/components/ConfirmedBooking.js
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom';
 import Header from './Header';
 import ConfirmationPage from './ConfirmationPage';
 import Footer from './Footer';
@@ -8,13 +7,6 @@ function ConfirmedBooking() {
     <>
       <Header />
       <ConfirmationPage />
-      <Link
-        to="/"
-        className="primary-button primary-button--yellow"
-        aria-label="homepage button"
-      >
-        Go To Homepage
-      </Link>
       <Footer />
     </>
   );
