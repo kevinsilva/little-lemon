c17eed75a68c11819dab91f6bb1b7776c63ed54a
kevinsilva <kevinsilva.j@gmail.com>
2024-02-19 15:23:54 +0000

    removes log

--- src/analytics/useAnalytics.js
+++ src/analytics/useAnalytics.js
@@ -4,7 +4,6 @@ import ReactGA from 'react-ga4';
 export default function useAnalytics() {
   useEffect(() => {
     ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_ID);
-    console.log(process.env.REACT_APP_GOOGLE_ANALYTICS_ID)
     ReactGA.send({ hitType: "pageview", page: window.location.pathname, title: "Little Lemon" });
   }, []);
 }
